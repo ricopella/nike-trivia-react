c3a8e2b4c7fcda626dde45dd92f3ea52eb57ef11
Narin Sundarabhaya <nrs710@gmail.com>
2019-07-20 18:23:21 -0700

    feature(questionsPage): Add answers
    * map answers  to questions as divs for now (WIP)

--- src/components/questions.js
+++ src/components/questions.js
@@ -6,7 +6,16 @@ const QuestionPage = () => {
   return (
     <Card>
       {questions.map((x, i) => {
-        return <div>{x.question}</div>;
+        return (
+          <div className="questionWrapper">
+            <h3>{x.question}</h3>
+            <div className="answersWrapper">
+              {x.answers.map(y => (
+                <div className="answer">{y}</div>
+              ))}
+            </div>
+          </div>
+        );
       })}
     </Card>
   );
