475cf0fd2bfca80e63ae157d586e7bb7fbfa0f27
Narin Sundarabhaya <nrs710@gmail.com>
2020-02-28 07:47:40 -0800

    feature(questions): add timer

--- src/components/questions.js
+++ src/components/questions.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./card";
 import createPlayerAnswerCache from "../utils/createPlayerAnswerCache";
 import sumPlayerScore from "../utils/sumPlayerScore";
@@ -8,6 +8,8 @@ import "../styles.css";
 import setUsersScore from "../utils/setUsersScore";
 import questions from "../data/questions";
 
+const TIMER_SECONDS = 60;
+
 const QuestionPage = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [currentSelectedAnswer, setCurrentSelectedAnswer] = useState(null);
@@ -15,6 +17,7 @@ const QuestionPage = () => {
   const [playerAnswersCache, setPlayersAnswersCache] = useState(() =>
     createPlayerAnswerCache()
   );
+  const [timeLeft, setTimeLeft] = useState(TIMER_SECONDS);
 
   const updateScore = async () => {
     const [updatedCache, answeredCount] = setUsersScore(
@@ -60,63 +63,81 @@ const QuestionPage = () => {
     );
   };
 
-  // TODO: add timer
+  useEffect(() => {
+    // exit early when we reach 0
+    if (!timeLeft) return;
+
+    // save intervalId to clear the interval when the
+    // component re-renders
+    const intervalId = setInterval(() => {
+      setTimeLeft(timeLeft - 1);
+    }, 1000);
+
+    // clear interval on re-render to avoid memory leaks
+    return () => clearInterval(intervalId);
+    // add timeLeft as a dependency to re-rerun the effect
+    // when we update it
+  }, [timeLeft]);
 
   return (
-    <div className="gameContainer">
-      {!finalScore ? (
-        <>
-          <div className="questionsQueueContainer">
-            {questions
-              .filter((x, i) => i < 5)
-              .map((x, i) => (
-                <ShoeBox
-                  number={i + 1}
-                  selected={currentQuestionIndex === i}
-                  handleClick={updateCurrentQuestions}
-                  index={i}
-                  key={`top_${i}`}
-                  isAnswered={playerAnswersCache[i].selected}
-                />
-              ))}
-          </div>
-          <Card>
-            <div className="questionWrapper">
-              {questions[currentQuestionIndex] && (
-                <ShoeboxLabel
-                  label={questions[currentQuestionIndex]}
-                  setSelectedAnswer={setSelectedAnswer}
-                  setQuestionIndex={setQuestionIndex}
-                  currentSelectedAnswer={currentSelectedAnswer}
-                  currentQuestionIndex={currentQuestionIndex}
-                />
-              )}
+    <div>
+      {/* TODO: style timer */}
+      <h1>{timeLeft}</h1>
+      <div className="gameContainer">
+        {!finalScore && timeLeft !== 0 ? (
+          <>
+            <div className="questionsQueueContainer">
+              {questions
+                .filter((x, i) => i < 5)
+                .map((x, i) => (
+                  <ShoeBox
+                    number={i + 1}
+                    selected={currentQuestionIndex === i}
+                    handleClick={updateCurrentQuestions}
+                    index={i}
+                    key={`top_${i}`}
+                    isAnswered={playerAnswersCache[i].selected}
+                  />
+                ))}
             </div>
-          </Card>
-          <div className="questionsQueueContainer">
-            {questions
-              .filter((x, i) => i >= 5)
-              .map((x, i) => (
-                <ShoeBox
-                  number={i + 1 + 5}
-                  selected={currentQuestionIndex === i + 5}
-                  index={i + 5}
-                  handleClick={updateCurrentQuestions}
-                  key={`bottom_${i}`}
-                />
-              ))}
-          </div>
-        </>
-      ) : (
-        // TODO: style final score
-        // add a reset button
-        <ShoeboxLabel
-          label={{ question: `Final Score:` }}
-          currentQuestionIndex={
-            !Number.isNaN(finalScore) && finalScore > 0 ? finalScore - 1 : 0
-          }
-        />
-      )}
+            <Card>
+              <div className="questionWrapper">
+                {questions[currentQuestionIndex] && (
+                  <ShoeboxLabel
+                    label={questions[currentQuestionIndex]}
+                    setSelectedAnswer={setSelectedAnswer}
+                    setQuestionIndex={setQuestionIndex}
+                    currentSelectedAnswer={currentSelectedAnswer}
+                    currentQuestionIndex={currentQuestionIndex}
+                  />
+                )}
+              </div>
+            </Card>
+            <div className="questionsQueueContainer">
+              {questions
+                .filter((x, i) => i >= 5)
+                .map((x, i) => (
+                  <ShoeBox
+                    number={i + 1 + 5}
+                    selected={currentQuestionIndex === i + 5}
+                    index={i + 5}
+                    handleClick={updateCurrentQuestions}
+                    key={`bottom_${i}`}
+                  />
+                ))}
+            </div>
+          </>
+        ) : (
+          // TODO: style final score
+          // add a reset button
+          <ShoeboxLabel
+            label={{ question: `Final Score:` }}
+            currentQuestionIndex={
+              !Number.isNaN(finalScore) && finalScore > 0 ? finalScore - 1 : 0
+            }
+          />
+        )}
+      </div>
     </div>
   );
 };
